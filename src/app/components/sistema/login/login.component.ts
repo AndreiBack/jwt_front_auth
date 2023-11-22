fc3424d1558a90c9c3fadaa76f221a31ce738bf9
AndreiBack <andreigregoriback@gmail.com>
2023-11-21 21:46:41 -0300

    funcionando autenticação login

---
+++ src/app/components/sistema/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/models/login';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  loginService = inject(LoginService);
+  login: Login = new Login();
+  roteador = inject(Router);
+
+  constructor() {
+    //remover o token aqui
+    this.loginService.removeToken();
+  }
+
+  logar() {
+    //implementar a requisição aqui e colocar o token no localstorage
+    this.loginService.logar(this.login).subscribe({
+      next: usuario => {localStorage.setItem("token", usuario.token),this.roteador.navigate(['admin/produtos']);
+    },
+      error: erro => {console.log(erro);
+      }
+    });
+
+   
+  }
+}
